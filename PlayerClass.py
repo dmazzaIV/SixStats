fbc4308a27bec1ed168dbbfbed5965a39cc0a2ca
Dan Mazza <d.mazza830@gmail.com>
2018-12-19 15:28:33 -0800

    populates player stats, NEXT: get operator stats

--- PlayerClass.py
+++ PlayerClass.py
@@ -28,13 +28,7 @@ class Player:
 		self.driver.switch_to.default_content()
 
 
-	def scrapeStats(self):
-
-		self.driver.get('https://game-rainbow6.ubi.com/en-us/uplay/player-statistics/dbd1cef3-d69d-4296-a235-ae8d7d70363f/multiplayer')
-		self.login()
-
-		#Implicit wait for webpage to load after loggining in
-		self.driver.implicitly_wait(5)
+	def scrapePlayerStats(self):
 
 		#Pull all stats from webpage into a list
 		#Particular stat value inedexes' differ if the person is ranked vs unranked so we need 2 cases
@@ -44,11 +38,37 @@ class Player:
 		if ranked.text == 'NOT RANKED YET.':
 			self.player_stats['Rank'] = 'Not Ranked'
 			self.player_stats['Time Played'] = stats_list[4].text
-			#self.player_stats['Headshot %'] = 
+			self.player_stats['Headshots']  = stats_list[8]
 			self.player_stats['W/L'] = stats_list[11].text
 			self.player_stats['K/D'] = stats_list[12].text
+			self.player_stats['Melee Kills'] = stats_list[10].text
+		else:
+			self.player_stats['Rank'] = stats_list[4].text
+			self.player_stats['Time Played'] = stats_list[6].text
+			self.player_stats['Headshots'] = stats_list[10]
+			self.player_stats['W/L'] = stats_list[13].text
+			self.player_stats['K/D'] = stats_list[14].text
+			self.player_stats['Melee Kills'] = stats_list[12].text
 
+	def scrapeOperatorStats(self):
+		#navigate to operator tab
+		operator_tab = self.driver.find_element_by_xpath('//*[@id="section"]/div/div/div[2]/div/div[1]/nav/ul/li[3]/span')
+		operator_tab.click()
+
+		operator_names = self.driver.find_elements_by_tag_name('h4')
+		for name in operator_names:
+			print(name.text)
+
+	def populateStats(self):
+		
+		self.driver.get('https://game-rainbow6.ubi.com/en-us/uplay/player-statistics/b3f64755-8892-47db-aa6c-82929cd29f30/multiplayer')
+		self.login()
+
+		#Implicit wait for webpage to load after loggining in
+		self.driver.implicitly_wait(5)
+		self.scrapePlayerStats()
+		#self.scrapeOperatorStats()
 
 
 Dan = Player('Dan')
-Dan.scrapeStats()
+Dan.populateStats()
