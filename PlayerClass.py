4af27bf479e67f95b74077c2e79899fe73221f2d
Dan Mazza <d.mazza830@gmail.com>
2018-12-21 15:13:11 -0800

    replaced most implicit waits with explicit waits to be more efficient, operator stats gets all but the first two ops

--- PlayerClass.py
+++ PlayerClass.py
@@ -13,14 +13,14 @@ class Player:
 
 	def login(self):
 
-		#login pop up is in a different IFrame, switch to it
-		iframe = self.driver.find_element_by_tag_name('iframe')
-		self.driver.switch_to.frame(iframe)
+		#login pop up is in a different IFrame
+		#Wait for it to load and switch to it
+		WebDriverWait(self.driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe.ng-scope.ng-isolate-scope.rs-template-uplay-connect")))
 
 		login_email = self.driver.find_element_by_id('AuthEmail')
-		login_email.send_keys('')
+		login_email.send_keys('d.mazza830@gmail.com')
 		login_pwd = self.driver.find_element_by_id('AuthPassword')
-		login_pwd.send_keys('')
+		login_pwd.send_keys('Frenchtoast#1')
 		submit_button = self.driver.find_element_by_id('LogInButton')
 		submit_button.click()
 
@@ -52,22 +52,31 @@ class Player:
 
 	def scrapeOperatorStats(self):
 		#navigate to operator tab
-		operator_tab = self.driver.find_element_by_xpath('//*[@id="section"]/div/div/div[2]/div/div[1]/nav/ul/li[3]/span')
-		operator_tab.click()
+		operator_tab = self.driver.find_element_by_xpath('//*[@id="section"]/div/div/div[2]/div/div[1]/div/div/div/div/article[1]/div[2]/div/div[1]/button')
+		self.driver.execute_script("arguments[0].click();", operator_tab)
+		#wait for operator stats elements to load
+		#TODO: figure out a way to explicitlly wait
+		self.driver.implicitly_wait(2)
 
-		operator_names = self.driver.find_elements_by_tag_name('h4')
-		for name in operator_names:
-			print(name.text)
+		#Get the li tag that is a list of all operators and thier respective stats
+		operator_list_set = self.driver.find_element_by_xpath('//*[@id="section"]/div/div/div[2]/div/div[1]/div/div/div/div/article[3]/div[1]/div/div/div/nav/ul')
+		operators = operator_list_set.find_elements_by_tag_name('li')
+
+		for operator in operators:
+			operator_stats = operator.find_elements_by_tag_name('p')
+			for stat in operator_stats:
+				print(stat.text)
 
 	def populateStats(self):
 		
 		self.driver.get('https://game-rainbow6.ubi.com/en-us/uplay/player-statistics/b3f64755-8892-47db-aa6c-82929cd29f30/multiplayer')
 		self.login()
 
-		#Implicit wait for webpage to load after loggining in
-		self.driver.implicitly_wait(5)
-		self.scrapePlayerStats()
-		#self.scrapeOperatorStats()
+		#Wait for operator stats button to be clickable
+		#This tells us that the page is fully loaded and we are able to navigate to the operator page when we are done with the player stats
+		WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#section > div > div > div.ng-scope > div > div.player-statistics-tabs.ng-isolate-scope.rs-organism-tabs > div > div > div > div > article.ng-scope.ng-isolate-scope.selected > div.player-statistics-most-used-operator.rs-atom-box > div > div.more-operator-stats.ng-scope > button')))
+		#self.scrapePlayerStats()
+		self.scrapeOperatorStats()
 
 
 Dan = Player('Dan')
