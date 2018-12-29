4e54434d770a9d4ebfeddd53d9eb0a584bd2d3d5
Dan Mazza <d.mazza830@gmail.com>
2018-12-29 14:12:06 -0800

    fixed naming convention for non-public methods in Player.py

--- Player.py
+++ Player.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 class Player:
 
 	def __init__(self, url, login_info):
-		self.login_info = login_info.copy()
+		self._login_info = login_info.copy()
 		self.url = url
 		#make chromedriver headless as we don't need the window to be displayed
 		#also makes it a tad bit faster since it is not loading grpahical elements
@@ -18,25 +18,25 @@ class Player:
 		self.player_stats = {}
 		self.operator_stats = {}
 
-		self.populate_stats()
+		self._populate_stats()
 
-	def login(self):
+	def _login(self):
 		"""Logs the driver into https://game-rainbow6.ubi.com for given player link."""
-		#login pop up is in a different IFrame
+		#_login pop up is in a different IFrame
 		#Wait for it to load and switch to it
 		WebDriverWait(self.driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe.ng-scope.ng-isolate-scope.rs-template-uplay-connect")))
 
 		login_email = self.driver.find_element_by_id('AuthEmail')
-		login_email.send_keys(self.login_info['email'])
+		login_email.send_keys(self._login_info['email'])
 		login_pwd = self.driver.find_element_by_id('AuthPassword')
-		login_pwd.send_keys(self.login_info['pwd'])
-		submit_button = self.driver.find_element_by_id('LogInButton')
+		login_pwd.send_keys(self._login_info['pwd'])
+		submit_button = self.driver.find_element_by_id('_loginButton')
 		self.driver.execute_script("arguments[0].click();", submit_button)
 
 		#Switch driver back to main webpage for webscraping
 		self.driver.switch_to.default_content()
 
-	def calculate_headshot_percent(self, total_headshots):
+	def _calculate_headshot_percent(self, total_headshots):
 		"""Returns headshot percentage for player as a float (headshots / kills).
 
 		Keyword arguements:
@@ -47,7 +47,7 @@ class Player:
 
 		return float(total_headshots) / float(total_kills)
 
-	def scrape_player_stats(self):
+	def _scrape_player_stats(self):
 		"""Driver scrapes general player stats(Rank,Time Played, Headshot %, W/L, K/D, Melee kills) and stores them in player_stats dictionary."""
 		#Pull all stats from webpage into a list
 		#Particular stat value inedexes' differ if the person is ranked vs unranked so we need 2 cases
@@ -57,19 +57,19 @@ class Player:
 		if ranked.text == 'NOT RANKED YET.':
 			self.player_stats['Rank'] = 'Not Ranked Yet'
 			self.player_stats['Time Played'] = stats_list[4].get_attribute('innerHTML')
-			self.player_stats['Headshot %']  = self.calculate_headshot_percent(stats_list[8].get_attribute('innerHTML'))
+			self.player_stats['Headshot %']  = self._calculate_headshot_percent(stats_list[8].get_attribute('innerHTML'))
 			self.player_stats['W/L'] = stats_list[11].get_attribute('innerHTML')
 			self.player_stats['K/D'] = stats_list[12].get_attribute('innerHTML')
 			self.player_stats['Melee Kills'] = stats_list[10].get_attribute('innerHTML')
 		else:
 			self.player_stats['Rank'] = stats_list[4].get_attribute('innerHTML')
 			self.player_stats['Time Played'] = stats_list[6].get_attribute('innerHTML')
-			self.player_stats['Headshot %']  = self.calculate_headshot_percent(stats_list[10].get_attribute('innerHTML'))
+			self.player_stats['Headshot %']  = self._calculate_headshot_percent(stats_list[10].get_attribute('innerHTML'))
 			self.player_stats['W/L'] = stats_list[13].get_attribute('innerHTML')
 			self.player_stats['K/D'] = stats_list[14].get_attribute('innerHTML')
 			self.player_stats['Melee Kills'] = stats_list[12].get_attribute('innerHTML')
 
-	def scrape_operator_stats(self):
+	def _scrape_operator_stats(self):
 		"""Driver scrapes stats for all the player's operators(Name,Time Played, W/L, K/D) and stores them in operator_stats dictionary."""
 		#Get the li tag that is a list of all operators and thier respective stats
 		operator_list_set = self.driver.find_element_by_xpath('//*[@id="section"]/div/div/div[2]/div/div[1]/div/div/div/div/article[3]/div[1]/div/div/div/nav/ul')
@@ -91,15 +91,15 @@ class Player:
 			self.operator_stats[operator_name.get_attribute('innerHTML')] = inner_dictionary
 
 
-	def populate_stats(self):
-		"""Calls the scrapPlayerStats() and scrape_operator_stats() functions to populate both the player_stats and operator_stats dictionaries."""FH
+	def _populate_stats(self):
+		"""Calls the scrapPlayerStats() and _scrape_operator_stats() functions to populate both the player_stats and operator_stats dictionaries."""FH
 		self.driver.get(self.url)
-		self.login()
+		self._login()
 
 		#wait for K/D to be visible, this is an arbitrary choice to just wait for the stats to load
 		WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#section > div > div > div.ng-scope > div > div.player-statistics-tabs.ng-isolate-scope.rs-organism-tabs > div > div > div > div > article.ng-scope.ng-isolate-scope.selected > div.player-statistics-main.rs-atom-box > div > div.statistic-group.overview-hero.ng-scope > div:nth-child(2) > div > div > div.hero-stats > div > div > div.stats > p.stat-value.ng-binding')))
-		self.scrape_player_stats()
-		self.scrape_operator_stats()
+		self._scrape_player_stats()
+		self._scrape_operator_stats()
 
 	
 	#ALL THE GETTER FUNCTIONS ARE DOWN HERE
